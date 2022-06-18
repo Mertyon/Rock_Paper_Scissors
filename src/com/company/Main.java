6dcc7a21c69302a7515fecd1293ef6c0f4435583
Mertyon <81264329+Mertyon@users.noreply.github.com>
2022-06-18 21:43:35 +0200

    It's alive!
    Working just right

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,90 @@
 package com.company;
+import java.util.Random;
+import java.util.Scanner;
+
+
 
 public class Main {
 
-    public static void main(String[] args) {
-System.out.println("Test");    
-    }
+        public static final String ROCK = "R";
+        public static final String PAPER = "P";
+        public static final String SCISSORS = "S";
+
+
+        //Get Result
+        public static void getResult(String usersMove, String computersMove) {
+            System.out.println("Computer's move is: " + computersMove);
+
+            if (usersMove.equals(computersMove))
+                System.out.println("It's a tie!");
+
+            //User == Rock
+            else if (usersMove.equals(ROCK))
+            {
+                if (computersMove.equals(SCISSORS))
+                    System.out.println("Rock crushes scissors. You win!!");
+                else if (computersMove.equals(PAPER))
+                    System.out.println("Paper eats rock. You lose!!");
+            }
+
+            //User == Paper
+            else if (usersMove.equals(PAPER))
+            {
+                if (computersMove.equals(ROCK))
+                    System.out.println("Paper eats rock. You win!!");
+                else if (computersMove.equals(SCISSORS))
+                    System.out.println("Scissor cuts paper.  You lose!!");
+            }
+
+            //User == Scissors
+            else if (usersMove.equals(SCISSORS))
+            {
+                if (computersMove.equals(PAPER))
+                    System.out.println("Scissor cuts paper.  You win!!");
+                else if (computersMove.equals(ROCK))
+                    System.out.println("Rock breaks scissors.  You lose!!");
+            }
+            else
+                System.out.println("Invalid user input.");
+        }
+
+
+        //Get Computer's move
+        public static String getComputersMove(){
+            int computersNum;
+            String computersMove="";
+            Random random = new Random();
+            computersNum = random.nextInt(3) + 1;
+            if (computersNum == 1)
+                computersMove = ROCK;
+            else if (computersNum == 2)
+                computersMove = PAPER;
+            else if (computersNum == 3)
+                computersMove = SCISSORS;
+            return computersMove;
+        }
+
+
+        //Get User's move | Auto changing into Uppercase
+        public static String getUsersMove(){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter your play: ");
+            String input = scanner.next().toUpperCase();
+            return input;
+        }
+
+
+        //Main method
+
+        public static void main(String[] args) {
+            System.out.println( "Rock, Paper, Scissors!\n"
+                    + "Please enter a move.\n"
+                    +"Rock = R, Paper= P, and Scissors = S.\n");
+            String userInput = getUsersMove();
+            if (userInput.equals(PAPER) || userInput.equals(ROCK) || userInput.equals(SCISSORS))
+                getResult(userInput, getComputersMove());
+            else
+                System.out.println("Invalid Input " + userInput);
+        }
 }
+
